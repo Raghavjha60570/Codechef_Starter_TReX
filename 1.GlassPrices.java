e2b2b7f27dddc2fc50b46eeac2dcd17e3188254f
Melisandre_64 <Rjha60571@gmail.com>
2024-10-24 07:36:44 +0530

    Create 1.GlassPrices.java

---
+++ 1.GlassPrices.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// easy one
+		Scanner sc = new Scanner ( System.in);
+		int x= sc.nextInt();
+		int  y= sc.nextInt();
+		if ( 2*x>=y){
+		    System.out.println("METAL");
+		}else {
+		    System.out.println("PLASTIC");
+		}
+
+	}
+}
